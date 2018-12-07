16250701509bdfe5758785f7771613c12b417ae9
mbrula <matiasbrula@gmail.com>
2018-12-07 14:14:42 -0300

    Test

--- src/game/backend/element/Gap.java
+++ src/game/backend/element/Gap.java
@@ -6,5 +6,5 @@ public class Gap extends Element {
     public boolean isMovable() { return false; }
 
     @Override
-    public String getKey() { return "GIL"; }
+    public String getKey() { return "GAP"; }
 }
