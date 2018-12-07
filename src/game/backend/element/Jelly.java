1ee0dda7568acee334fdd0b53dca9b46eac6e4cc
JulianVuoso <juvuoso@itba.edu.ar>
2018-12-07 18:03:56 -0300

    Jelly

---
+++ src/game/backend/element/Jelly.java
@@ -0,0 +1,13 @@
+package game.backend.element;
+
+public class Jelly extends Element{
+    @Override
+    public boolean isMovable() {
+        return false;
+    }
+
+    @Override
+    public String getKey() {
+        return "JELLY";
+    }
+}
