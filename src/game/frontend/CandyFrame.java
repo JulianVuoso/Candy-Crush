eebd5e4981fc735b602ddd94dd7111bd13d82a49
ntallar <nicolastallar@gmail.com>
2018-12-07 13:52:52 -0300

    Change Cell Size

--- src/game/frontend/CandyFrame.java
+++ src/game/frontend/CandyFrame.java
@@ -15,7 +15,7 @@ import javafx.util.Duration;
 
 public class CandyFrame extends VBox {
 
-	private static final int CELL_SIZE = 100;
+	private static final int CELL_SIZE = 65;
 
 	private BoardPanel boardPanel;
 	private ScorePanel scorePanel;
